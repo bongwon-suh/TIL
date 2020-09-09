80dd1dee13b3d52aff43cfb722d885baf47b6389
bongwon-suh <suhliebe@gmail.com>
2020-09-09 17:10:12 +0900

    2020. 09. 09

---
+++ c++/20200909/chapter08/ex02_copy.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+class MyArray{
+public:
+    int size;
+    int *data;
+    
+    MyArray(int size){
+        this->size = size;
+        data = new int[size];
+    }
+    ~MyArray(){
+        if(data!=NULL){
+            delete []data;
+        }
+    }
+};
+
+int main(int argc, char const *argv[]) {
+    MyArray buffer(10);
+    buffer.data[0]=1;
+    {
+        MyArray clone = buffer; //복사 생성자 호출
+    }   // clone 삭제
+
+    buffer.data[0]=2;
+    return 0;
+}
