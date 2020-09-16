075ce4d4098f0c301b0cc15f40cbba6017ea535f
bongwon-suh <suhliebe@gmail.com>
2020-09-16 18:54:15 +0900

    dht11

---
+++ arduino/source/dht11/ex01_dht11/app.ino
@@ -0,0 +1,27 @@
+#include <DHT.h>
+#include "MiniCom.h"
+#define DHTPIN 3
+#define DHTTYPE DHT11
+
+DHT dht11(DHTPIN, DHTTYPE);
+MiniCom com;
+
+void checkHumiTemp(){
+    float h = dht11.readHumidity();
+    float t = dht11.readTemperature();
+
+    com.print(0, "Humi", h);
+    com.print(1, "Temp", t);
+}
+
+void setup(){
+    com.init();
+    com.setInterval(2000, checkHumiTemp);
+    com.print(0, "MiniCom start");
+    com.print(1,"Humi/Temp ex" );
+    dht11.begin();
+}
+
+void loop(){
+    com.run();
+}
