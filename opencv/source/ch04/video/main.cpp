dda77affcf5c096fb0ed1746809005d95ddfb188
bongwon-suh <suhliebe@gmail.com>
2020-09-24 02:00:37 +0900

    camera in video out

--- opencv/source/ch04/video/main.cpp
+++ opencv/source/ch04/video/main.cpp
@@ -63,8 +63,48 @@ void video_in() {
 	destroyAllWindows();
 }
 
+void camera_in_video_out() {
+	VideoCapture cap(0);
+
+	if (!cap.isOpened()) {
+		cerr << "camera open failed" << endl;
+		return;
+	}
+
+	int w = cvRound(cap.get(CAP_PROP_FRAME_WIDTH));
+	int h = cvRound(cap.get(CAP_PROP_FRAME_HEIGHT));
+	double fps = cap.get(CAP_PROP_FPS);
+
+	int fourcc = VideoWriter::fourcc('D', 'I', 'V', 'X');
+	int delay = cvRound(1000 / fps);
+	
+	VideoWriter outputVideo("output.avi", fourcc, fps, Size(640, 480), true);
+
+	if (!outputVideo.isOpened()) {
+		cout << "File open failed" << endl;
+		return;
+	}
+	
+	Mat frame, inversed;
+	while (true) {
+		cap >> frame;
+		if (frame.empty())
+			break;
+		inversed = ~frame;
+		outputVideo << inversed;
+
+		imshow("frame", frame);
+		imshow("inversed", inversed);
+
+		if (waitKey(delay) == 27)
+			break;
+	}
+	destroyAllWindows();
+}
+
 int main() {
 	//camera_in();
-	video_in();
+	//video_in();
+	camera_in_video_out();
 	return 0;
 }
