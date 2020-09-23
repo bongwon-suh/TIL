124c1746697ee3b3c9242a8de9cec88cc7bb74f8
bongwon-suh <suhliebe@gmail.com>
2020-09-24 00:13:48 +0900

    video in

--- opencv/source/ch04/video/main.cpp
+++ opencv/source/ch04/video/main.cpp
@@ -31,7 +31,40 @@ void camera_in() {
 
 }
 
+void video_in() {
+	VideoCapture cap("stopwatch.avi");
+
+	if (!cap.isOpened()) {
+		cerr << "video open failed" << endl;
+		return;
+	}
+
+	cout << "frame width : " << cvRound(cap.get(CAP_PROP_FRAME_WIDTH)) << endl;
+	cout << "frame height : " << cvRound(cap.get(CAP_PROP_FRAME_HEIGHT)) << endl;
+	cout << "frame count : " << cvRound(cap.get(CAP_PROP_FRAME_COUNT)) << endl;
+
+	double fps = cap.get(CAP_PROP_FPS);
+	cout << "FPS : " << fps << endl;
+
+	int delay = cvRound(1000 / fps);
+
+	Mat frame, inversed;
+	while (true) {
+		cap >> frame;
+		if (frame.empty())
+			break;
+		inversed = ~frame;
+		imshow("frame", frame);
+		imshow("inversed", inversed);
+
+		if (waitKey(delay) == 27)
+			break;
+	}
+	destroyAllWindows();
+}
+
 int main() {
-	camera_in();
+	//camera_in();
+	video_in();
 	return 0;
 }
