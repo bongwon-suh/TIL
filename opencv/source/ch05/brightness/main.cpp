8911b3d9fc6f75f6089eabbeb6b4c067e8dd552f
bongwon-suh <suhliebe@gmail.com>
2020-10-01 21:55:34 +0900

    brightness source

---
+++ opencv/source/ch05/brightness/main.cpp
@@ -0,0 +1,28 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+void brightness1() {
+	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
+
+	if (src.empty()) {
+		cerr << "Image Load failed" << endl;
+		return;
+	}
+
+	Mat dst = src + 100;
+
+	imshow("src", src);
+	imshow("dst", dst);
+	waitKey();
+
+	destroyAllWindows();
+}
+
+int main() {
+	brightness1();
+
+	return 0;
+}
