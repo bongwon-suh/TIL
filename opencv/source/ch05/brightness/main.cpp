67011d37f4ac0e5095082d697f17059318951f59
bongwon-suh <suhliebe@gmail.com>
2020-10-01 22:28:38 +0900

    brightness source

--- opencv/source/ch05/brightness/main.cpp
+++ opencv/source/ch05/brightness/main.cpp
@@ -4,6 +4,8 @@
 using namespace cv;
 using namespace std;
 
+void on_brightness(int pos, void* userdata);
+
 void brightness1() {
 	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
 
@@ -21,8 +23,80 @@ void brightness1() {
 	destroyAllWindows();
 }
 
-int main() {
-	brightness1();
+void brightness2() {
+	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
 
+	if (src.empty()) {
+		cerr << "Image Load failed" << endl;
+		return;
+	}
+
+	Mat dst(src.rows, src.cols, src.type());
+
+	for (int j = 0; j < src.rows; j++) {
+		for (int i = 0; i < src.cols; i++) {
+			dst.at<uchar>(j, i) = src.at<uchar>(j, i) + 100;
+		}
+	}
+
+	imshow("src", src);
+	imshow("dst", dst);
+	waitKey();
+
+	destroyAllWindows();
+}
+
+void brightness3() {
+	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
+
+	if (src.empty()) {
+		cerr << "Image Load failed" << endl;
+		return;
+	}
+
+	Mat dst(src.rows, src.cols, src.type());
+
+	for (int j = 0; j < src.rows; j++) {
+		for (int i = 0; i < src.cols; i++) {
+			dst.at<uchar>(j, i) = saturate_cast<uchar>(src.at<uchar>(j, i) + 100);
+		}
+	}
+
+	imshow("src", src);
+	imshow("dst", dst);
+	waitKey();
+
+	destroyAllWindows();
+}
+
+void brightness4() {
+	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
+
+	if (src.empty()) {
+		cerr << "Image Load failed" << endl;
+		return;
+	}
+
+	namedWindow("dst");
+	createTrackbar("Brightness", "dst", 0, 100, on_brightness, (void*)&src);
+
+	on_brightness(0, (void*)&src);
+
+	waitKey();
+	destroyAllWindows();
+}
+
+void on_brightness(int pos, void* userdata) {
+	Mat src = *(Mat*)userdata;
+	Mat dst = src + pos;
+
+	imshow("dst", dst);
+}
+
+int main() {
+	// brightness1();
+	// brightness2();
+	// brightness3();
+	brightness4();
 	return 0;
 }
