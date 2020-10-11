5a5e1718dfce7c489bf74076f6c3f1f67691d6cd
bongwon-suh <suhliebe@gmail.com>
2020-10-11 21:43:13 +0900

    gaussian filter source

--- opencv/source/ch07/blurring/main.cpp
+++ opencv/source/ch07/blurring/main.cpp
@@ -27,7 +27,30 @@ void blurring_mean() {
 	destroyAllWindows();
 }
 
+void blurring_gaussian() {
+	Mat src = imread("rose.bmp", IMREAD_GRAYSCALE);
+
+	if (src.empty()) {
+		cerr << "image load fail" << endl;
+		return;
+	}
+
+	imshow("src", src);
+	
+	Mat dst;
+	for (int sigma = 1; sigma <= 5; sigma++) {
+		GaussianBlur(src, dst, Size(), (double)sigma);
+
+		String text = format("sigma=%d", sigma);
+		putText(dst, text, Point(10, 30), FONT_HERSHEY_SIMPLEX, 1.0, Scalar(255), 1, LINE_AA);
+		imshow("dst", dst);
+		waitKey(0);
+	}
+	destroyAllWindows();
+}
+
 int main() {
-	blurring_mean();
+	//blurring_mean();
+	blurring_gaussian();
 	return 0;
 }
